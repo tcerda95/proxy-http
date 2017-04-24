5b08b7ce0e57bdc920eb7f4a28bd30e0dff12fc1
tcerdaITBA <tcerda@itba.edu.ar>
2017-04-23 21:44:05 -0300

    Agregado \r

--- proxy-http/src/main/java/tp/pdc/proxy/HttpResponse.java
+++ proxy-http/src/main/java/tp/pdc/proxy/HttpResponse.java
@@ -3,8 +3,8 @@ package tp.pdc.proxy;
 import java.nio.charset.Charset;
 
 public enum HttpResponse {
-	BAD_REQUEST_400("HTTP/1.1 400 Bad Request\n"),
-	BAD_GATEWAY_502("HTTP/1.1 502 Bad Gateway\n");
+	BAD_REQUEST_400("HTTP/1.1 400 Bad Request\r\n"),
+	BAD_GATEWAY_502("HTTP/1.1 502 Bad Gateway\r\n");
 	
 	private byte[] response;
 	
