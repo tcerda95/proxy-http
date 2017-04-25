6b0c9a2793ad781b7d7cd117094f0474a905c5aa
Luciano Bianchi <lbianchi@itba.edu.ar>
2017-04-25 20:26:09 -0300

    Quiero mergear. Tests

--- proxy-http/src/test/java/tp/pdc/proxy/parserTest/HttpVersionParserTest.java
+++ proxy-http/src/test/java/tp/pdc/proxy/parserTest/HttpVersionParserTest.java
@@ -42,13 +42,29 @@ public class HttpVersionParserTest implements AsciiConstants {
         parser.parse(b, o);
     }
 
-    public void majorVersionTest() throws ParserFormatException {
-        HttpVersionParser parser = new HttpVersionParserImpl((byte) CR);
-        String s = "HTTP/1.1" + LF;
-
-        ByteBuffer b = ByteBuffer.wrap(s.getBytes());
-        ByteBuffer o = ByteBuffer.allocate(64);
-
-        parser.parse(b, o);
-    }
+//    public void majorVersionTest() throws ParserFormatException {
+//        HttpVersionParser parser1 = new HttpVersionParserImpl((byte) CR);
+//        HttpVersionParser parser2 = new HttpVersionParserImpl((byte) CR);
+//        HttpVersionParser parser3 = new HttpVersionParserImpl((byte) CR);
+//
+//        String s1 = "HTTP/1.1" + CR;
+//        String s2 = "HTTP/009.1" + CR;
+//        String s3 = "HTTP/0170.1" + CR;
+//
+//        ByteBuffer b1 = ByteBuffer.wrap(s1.getBytes());
+//        ByteBuffer b2 = ByteBuffer.wrap(s2.getBytes());
+//        ByteBuffer b3 = ByteBuffer.wrap(s3.getBytes());
+//
+//        ByteBuffer o = ByteBuffer.allocate(64);
+//
+//        parser1.parse(b1, o);
+//        assertEquals(parser1.getMajorHttpVersion(), 1);
+//
+//        o.clear();
+//
+//        parser2.parse(b2, o);
+//        assertEquals(parser2.getMajorHttpVersion(), 9);
+//
+//        o.clear();
+//    }
 }
