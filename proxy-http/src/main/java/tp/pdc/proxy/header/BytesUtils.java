ac41240dfedf1c1a5f626d2e9e1f2436a694001b
tcerdaITBA <tcerda@itba.edu.ar>
2017-04-25 14:12:11 -0300

    Unas cosas de los enums y bytes

--- proxy-http/src/main/java/tp/pdc/proxy/header/BytesUtils.java
+++ proxy-http/src/main/java/tp/pdc/proxy/header/BytesUtils.java
@@ -1,31 +1,34 @@
 package tp.pdc.proxy.header;
 
 import java.nio.ByteBuffer;
-import java.util.Map;
-import java.util.Set;
 
 public class BytesUtils {
 
-    public static <T> T getByBytes(Set<Map.Entry<byte[],T>> set, ByteBuffer bytes, int length) {
-        boolean equals;
-
-        for (Map.Entry<byte[], T> e: set) {
-            equals = true;
-            if (length != e.getKey().length)
-                continue;
-
-            bytes.mark();
-            for (int i = 0; i < length && equals; i++) {
-                if (bytes.get() != e.getKey()[i]) {
-                    equals = false;
-                }
-            }
-            bytes.reset();
-
-            if (equals)
-                return e.getValue();
-        }
-
-        return null;
+     public static boolean equalsBytes(byte[] array, ByteBuffer byteBuffer, int length) {
+    	byte[] bufferArray = byteBuffer.array();
+    	int offset = byteBuffer.position();
+    	
+    	if (length == array.length && equalsBytes(array, 0, bufferArray, offset, length))
+    		return true;
+    	
+    	return false;
+    }
+    
+    public static boolean equalsBytes(byte[] arr1, byte[] arr2, int length) {
+    	return equalsBytes(arr1, 0, arr2, 0, length);
+    }
+    
+    public static boolean equalsBytes(byte[] arr1, int offset1, byte[] arr2, int offset2, int length) {
+    	if (arr1.length - offset1 < length)
+    		return false;
+    	
+    	if (arr2.length - offset2 < length)
+    		return false;
+    	
+    	for (int i = offset1, j = offset2; length > 0; i++, j++, length--)
+    		if (arr1[i] != arr2[j])
+    			return false;
+    	
+    	return true;
     }
 }
