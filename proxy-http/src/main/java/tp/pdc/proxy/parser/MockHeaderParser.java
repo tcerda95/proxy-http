6085fbb4dac59e430948717c4a06e0680755b33f
tcerdaITBA <tcerda@itba.edu.ar>
2017-04-26 22:53:34 -0300

    Ahora si

---
+++ proxy-http/src/main/java/tp/pdc/proxy/parser/MockHeaderParser.java
@@ -0,0 +1,40 @@
+package tp.pdc.proxy.parser;
+
+import java.nio.ByteBuffer;
+
+import tp.pdc.proxy.exceptions.ParserFormatException;
+import tp.pdc.proxy.header.Header;
+import tp.pdc.proxy.header.Method;
+import tp.pdc.proxy.parser.interfaces.HttpRequestParser;
+
+public class MockHeaderParser implements HttpRequestParser {
+	
+	private boolean finished = false;
+
+	@Override
+	public boolean parse(ByteBuffer input, ByteBuffer output) throws ParserFormatException {
+		output.put(input);
+		finished = true;
+		return hasFinished();
+	}
+
+	@Override
+	public boolean hasFinished() {
+		return finished;
+	}
+
+	@Override
+	public boolean hasHeaderValue(Header header) {
+		return false;
+	}
+
+	@Override
+	public byte[] getHeaderValue(Header header) {
+		return null;
+	}
+
+	@Override
+	public boolean hasMethod(Method method) {
+		return false;
+	}
+}
---
+++ proxy-http/src/test/java/tp/pdc/proxy/parser/HttpRequestParserImplTest.java
@@ -0,0 +1,37 @@
+package tp.pdc.proxy.parser;
+
+import static org.junit.Assert.*;
+
+import java.io.UnsupportedEncodingException;
+import java.nio.ByteBuffer;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import tp.pdc.proxy.exceptions.ParserFormatException;
+
+public class HttpRequestParserImplTest {
+
+	private HttpRequestParserImpl parser;
+	private ByteBuffer inputBuffer;
+	private ByteBuffer outputBuffer;
+	
+	@Before
+	public void setUp() throws Exception {
+		parser = new HttpRequestParserImpl();
+		outputBuffer = ByteBuffer.allocate(4000);
+	}
+
+	@Test
+	public void testFinished() throws UnsupportedEncodingException, ParserFormatException {
+		String request =   "GET / HTTP/1.1\r\n"
+				 + "Host: localhost:8080\r\n"
+				 + "\r\n";
+		
+		inputBuffer = ByteBuffer.wrap(request.getBytes("ASCII"));
+		
+		assertTrue(parser.parse(inputBuffer, outputBuffer));
+		assertTrue(parser.hasFinished());
+	}
+
+}
