b2765f4ab04752a045115f7bf207596dd1915177
Carla Barruffaldi <cbarruffaldi@itba.edu.ar>
2017-06-08 20:56:08 -0300

    Docs

--- proxy-http/src/main/java/tp/pdc/proxy/metric/interfaces/HostMetric.java
+++ proxy-http/src/main/java/tp/pdc/proxy/metric/interfaces/HostMetric.java
@@ -1,15 +1,42 @@
 package tp.pdc.proxy.metric.interfaces;
 
+/**
+ * Metrics related to a specific host.
+ */
 public interface HostMetric {
+
+	/**
+	 * Get amount of connections
+	 * @return quantity of connections
+     */
 	public long getConnections ();
 
+	/**
+	 * Get bytes read from a certain host.
+	 * @return amount of bytes read
+     */
 	public long getBytesRead ();
 
+	/**
+	 * Get bytes written to a certain host
+	 * @return amount of bytes written
+     */
 	public long getBytesWritten ();
 
+	/**
+	 * Add one to the amount of connections made to a certain host
+	 */
 	public void addConnection ();
 
+	/**
+	 * Add a quantity of bytes read from a certain host
+	 * @param bytes read from a host
+     */
 	public void addBytesRead (long bytes);
 
+	/**
+	 * Add a quantity of bytes written to a certain host
+	 * @param bytes written to a host
+	 */
 	public void addBytesWritten (long bytes);
 }
